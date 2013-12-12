e3c2d37ad02b0c1215ddec9bf2a46445f68a34c1
Jan Wiegelmann <git@wiegelmann.net>
2013-12-12 17:44:08 +0100

    Release v1.0

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 secure.contactform.php
 ======================
 
-PGP contact form for your website in php
+A secure contact form made with PHP based on OpenPGP.js a JavaScript implementation of the OpenPGP standard.
+
+### Instructions
+
+Follow these instructions to get the form working:
+
+1. Open the **send_contact.php** file and replace in line 11 **your@mail.com** with the email address you wish to receive messages. 
+2. Open the **contact.html** file and replace in line 66 **-----BEGIN PGP PUBLIC KEY BLOCK-----** with your public PGP key.
+3. Upload all files to a server with the PHP mail() extension installed to use.
+
+### Demo
+
+[Click here](http://wiegelmann.net/contact.html "Demo") to see the secure contact form.
+
+### Changelog
+
+v1.0 Release
+
---
+++ send_form.php
@@ -0,0 +1,65 @@
+<?php
+
+/*
+ * secure.contactform.php v1.0
+ * Copyright (c) 2013 Jan Wiegelmann, http://wiegelmann.net
+ * open sourced under the MIT license.
+ * based on OpenPGP.js a JavaScript implementation of the OpenPGP standard.
+ */
+
+// your email address
+$email_to = "your@mail.com"; 
+
+if(isset($_POST['email'])) {
+ 
+    function died($error) {
+        echo "Sorry but there are error(s):<br /><br />";
+        echo $error."<br /><br />";
+        echo "Please go back and try again.<br /><br />";
+        die();
+    }
+ 
+    if(!isset($_POST['email']) || !isset($_POST['subject']) || !isset($_POST['message'])) { 
+        died('We are sorry, but there appears to be a problem with the form you submitted.');       
+    }
+ 
+    $email_from = $_POST['email']; 
+    $subject = $_POST['subject']; 
+    $message = $_POST['message']; 
+    $error_message = "";
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+ 
+  if(!preg_match($email_exp,$email_from)) { 
+    $error_message .= 'The Email Address you entered does not appear to be valid.<br />'; 
+  }
+ 
+  $string_exp = "/^[A-Za-z .'-]+$/";
+ 
+  if(!preg_match($string_exp,$subject)) {
+   	$error_message .= 'The Subject you entered does not appear to be valid.<br />';
+  } 
+  if(strlen($message) < 1) { 
+    $error_message .= 'The Message you entered do not appear to be valid.<br />'; 
+  } 
+  if(strlen($error_message) > 0) { 
+    died($error_message); 
+  } 
+  $email_message = "Form details below.\n\n";
+ 
+  function clean_string($string) {
+      $bad = array("content-type","bcc:","to:","cc:","href");
+      return str_replace($bad,"",$string);
+  }
+ 
+ 
+$header = 'From: ' . $email_from . "\r\n" . 'X-Mailer: PHP/' . phpversion();
+ 
+@mail($email_to, $subject, $message, $header);  
+ 
+?>
+
+Thank you for contacting us.
+ 
+<?php
+}
+?>
